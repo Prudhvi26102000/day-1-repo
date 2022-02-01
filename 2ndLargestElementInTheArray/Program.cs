b88c3e446f6a5ac149f1cfb92be0ad70adb51fd9
Prudhvi26102000 <dasariprudhvisai26@gmail.com>
2022-02-01 16:38:05 +0530

    new Commit

---
+++ 2ndLargestElementInTheArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _2ndLargestElementInTheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter length of the array");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int l1=0,l2=0;
+            for(int i=0;i<n;i++)
+            {
+                if (a[i] > l1)
+                {
+                    l1 = a[i];
+                }
+            }
+            for(int i=0;i<n;i++)
+            {
+                if(a[i]>l2 && a[i]!=l1)
+                {
+                    l2 = a[i];
+                }
+            }
+            Console.WriteLine("Second Largest element in the array is "+l2);
+
+        }
+    }
+}
---
+++ count/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 0;
+            Console.WriteLine("Enter length of the array");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a= new int[n];
+            for(int i=0;i<n;i++)
+            {
+                a[i]= Convert.ToInt32(Console.ReadLine());
+            }
+            int[] b = new int[256];
+            for(int i=0;i<n;i++)
+            {
+                b[a[i]] += 1;
+            }
+            for(int i=0;i<b.Length;i++)
+            {
+                if(b[i]>1)
+                {
+                    c += 1;
+                }
+            }
+            Console.WriteLine("Repeated elements in an array "+c);
+
+        }
+    }
+}
